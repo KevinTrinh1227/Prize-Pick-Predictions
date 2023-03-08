cfd5ad57382649f76556e0623457f6eaec6fe0fc
KevinTrinh2021 <kevintrinh1227@gmail.com>
2023-03-07 18:54:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,35 +20,31 @@ driver.find_element(By.XPATH, "//div[@class='name'][normalize-space()='NBA']").c
 
 time.sleep(2)
 
-driver.find_element(By.XPATH, "//div[@class='stat '][normalize-space()='Rebounds']").click()
-
 # Wait for the stat-container element to be present and visible
 stat_container = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "stat-container")))
 
 # Find all stat elements within the stat-container
 stat_elements = driver.find_elements(By.CSS_SELECTOR, "div.stat")
 
-# Initialize empty list to store data
-nbaPlayers = []
-
 # Iterate over each stat element
 for stat in stat_elements:
-    # Click the stat element
-    stat.click()
 
-    projections = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".projection")))
-    print("\n\n====================================================")
+    try:
+        # Click the stat element
+        stat.click()
 
-    for projection in projections:
-
-        names = projection.find_element(By.XPATH, './/div[@class="name"]').text
-        points= projection.find_element(By.XPATH, './/div[@class="presale-score"]').get_attribute('innerHTML')
-        text = projection.find_element(By.XPATH, './/div[@class="text"]').text
-        print(f"Player: {names}  -  Strike Value: {points} pts")
+        projections = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".projection")))
+        print("\n\n=================================================================")
+        
+        for projection in projections:
+            names = projection.find_element(By.XPATH, './/div[@class="name"]').text
+            points = projection.find_element(By.XPATH, './/div[@class="presale-score"]').get_attribute('innerHTML')
+            print(f"Player: {names:30} Strike Value: {points} pts")
+            
+    except:
+        pass
 
    
-    print("====================================================\n")
-df = pd.DataFrame(nbaPlayers)
-print(df)
+print("===================================================================\n")
 
 driver.quit()
