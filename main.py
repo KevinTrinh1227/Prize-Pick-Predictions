b8d26e9cf3d0cbfa3844b6b425a2eedad0b27b94
KevinTrinh2021 <kevintrinh1227@gmail.com>
2023-03-28 05:18:35 -0500

    Made output in table format

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 
 import json
 import numpy as np
+from tabulate import tabulate
 
 pre_json = "pre_formatted_projections.json" #where we copied and paste api into
 post_json = "post_formatted_projections.json" #after it gets cleaned up & formatted
@@ -19,7 +20,6 @@ post_json = "post_formatted_projections.json" #after it gets cleaned up & format
 
 # reads the pre_formmatted json file
 with open(pre_json, "r") as file:
-    # Load the JSON data into a Python dictionary
     data = json.load(file)
     # Format the JSON with indentation
     json_str = json.dumps(data, indent=4)
@@ -64,12 +64,40 @@ with open(post_json, 'w') as f:
     json.dump(results, f, indent=2)
 with open(post_json, 'r') as f:
     data = json.load(f)
-    
+
+#lengths and member counts
 num_players = len(data)
-print(f"Total number of players: {num_players}\n")
-"""
-for key in data:
-    name = data[key]['name']
-    team_name = data[key]['attributes']['team_name']
-    print(name, team_name)
-"""
+players_printed = 0
+
+table = []
+with open('output.txt', 'w') as f:
+    for idx, key in enumerate(data):
+        # the attribute values
+        name = data[key]['name']
+        team_name = data[key]['attributes']['team_name']
+        team_city_state = data[key]['attributes']['market']
+        
+        # strike value line scores
+        for item in data[key]['strike_values']:
+            if item['stat_type'] == 'Points':
+                points = item['line_score']
+            elif item['stat_type'] == 'Turnovers':
+                turnovers = item['line_score']
+            elif item['stat_type'] == 'Rebounds':
+                rebounds = item['line_score']
+            elif item['stat_type'] == 'Pts+Asts':
+                points_assists = item['line_score']
+            elif item['stat_type'] == 'Pts+Rebs':
+                points_rebounds = item['line_score']
+            elif item['stat_type'] == 'Pts+Rebs+Asts':
+                points_rebounds_assists = item['line_score']
+        
+        table.append([idx+1, name, team_city_state, team_name, points, turnovers, rebounds, points_assists, points_rebounds, points_rebounds_assists])
+        players_printed += 1
+    f.write(tabulate(table, headers=['##', 'Name', 'Market', 'Team Name', 'Pts', "T.O", "Rebs", "Pts+Ast", "Pts+Rebs", "Pts+Rebs+Ast"], tablefmt='orgtbl') + "\n")
+    
+print(tabulate(table, headers=['##', 'Name', 'Market', 'Team Name', 'Pts', "T.O", "Rebs", "Pts+Ast", "Pts+Rebs", "Pts+Rebs+Ast"], tablefmt='orgtbl') + "\n")
+
+
+print(f"A total of {num_players} player objects in json file.")
+print(f"{players_printed}/{num_players} were printed out in table format.\n\n")
