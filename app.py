352224ff7cacf22c4805e30a77c1bad5682b4e69
KevinTrinh2021 <kevintrinh1227@gmail.com>
2023-04-24 02:37:36 -0500

    updates to styling and index

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
 
-with open('json files/points_assists.json') as f:
+# Load the default JSON file
+with open('json files/points.json') as f:
     data = json.load(f)
 
-
+# Route for the home page
 @app.route('/')
 def index():
+    # Check if a data source parameter was passed in the URL
+    data_source = request.args.get('data_source', 'points')
+    if data_source == 'points':
+        with open('json files/points.json') as f:
+            data = json.load(f)
+    elif data_source == 'rebounds':
+        with open('json files/rebounds.json') as f:
+            data = json.load(f)
+    elif data_source == 'assists':
+        with open('json files/assists.json') as f:
+            data = json.load(f)
+    elif data_source == 'pts_asts':
+        with open('json files/points_assists.json') as f:
+            data = json.load(f)
+    elif data_source == 'pts_rebs':
+        with open('json files/points_rebounds.json') as f:
+            data = json.load(f)
+    elif data_source == 'pts_rebs_asts':
+        with open('json files/points_assists_rebounds.json') as f:
+            data = json.load(f)
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
