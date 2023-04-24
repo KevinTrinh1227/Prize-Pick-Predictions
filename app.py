f148652e6bd2de25033346ea6da808eb64a30352
KevinTrinh2021 <kevintrinh1227@gmail.com>
2023-04-23 23:34:23 -0500

    Edited index.html

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
+
 import json
 
 app = Flask(__name__)
 
-with open('json files/recommendations.json') as f:
+with open('json files/points.json') as f:
     data = json.load(f)
+
 
 @app.route('/')
 def index():
-    players = []
-    for player in data:
-        player_data = list(player.values())[0]
-        player_name = list(player.keys())[0]
-        player_image = player_data['general']['picture_link']
-        players.append({'name': player_name, 'image': player_image})
-    return render_template('index.html', players=players)
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
