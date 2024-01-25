8e73ddee2106b6e960b0e2d26b124fb31c2383b4
KevinTrinh1227 <kevintrinh1227@gmail.com>
2024-01-25 02:14:00 -0600

    Updates

--- main.py
+++ main.py
@@ -54,12 +54,11 @@ Download the correct version here: https://github.com/mozilla/geckodriver/releas
 gecko_path = './drivers/geckodriver.exe'  # Replace with the correct path to your GeckoDriver
 service = Service(gecko_path)
 
-"""
-Windows driver creation
-# firefox_options = Options()
-# firefox_options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'  # Update with your Firefox binary location
-# driver = webdriver.Firefox(service=service, options=firefox_options)
-"""
+# Windows driver creation
+firefox_options = Options()
+firefox_options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'  # Firefox binary location
+driver = webdriver.Firefox(service=service, options=firefox_options)
+
 
 # Linux Ubuntu driver creation
 driver = webdriver.Firefox()
